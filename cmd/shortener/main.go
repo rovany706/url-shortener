4a3c8b1c21c7b40cacee0a32a10213bca0610402
Ivan Ryazanov <rovany706@gmail.com>
2025-05-06 20:04:29 +0300

    Add build variables

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 
 	"go.uber.org/zap"
@@ -11,9 +12,17 @@ import (
 	"github.com/rovany706/url-shortener/internal/server"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 var appConfig *config.AppConfig
 
 func main() {
+	printBuildInfo()
+
 	var err error
 	appConfig, err = config.ParseArgs(os.Args[0], os.Args[1:])
 
@@ -46,4 +55,14 @@ func run(server *server.Server) (err error) {
 	}()
 
 	return err
+}
+
+func printBuildInfo() {
+	buildInfoMessageFmt := "Build version: %s\nBuild date: %s\nBuild commit: %s\n"
+	fmt.Printf(
+		buildInfoMessageFmt,
+		buildVersion,
+		buildDate,
+		buildCommit,
+	)
 }
